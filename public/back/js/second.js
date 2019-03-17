db1515c8573e7b8bbb2b0b0a56dba5cf58922aa4
790573490 <790573490@qq.com>
2019-03-17 21:08:38 +0800

    二级分类设置隐藏域进行提交完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -4,7 +4,7 @@ $(function () {
     var pageSize = 5;
 
 
-    //一进入页面发送ajax请求，渲染页面
+    //1一进入页面发送ajax请求，渲染页面
     render();
     function render() {
         $.ajax({
@@ -35,4 +35,54 @@ $(function () {
             }
         })
     }
+
+
+    /*2点击添加按钮，显示添加模态框*/
+    $("#addBtn").click(function () {
+        $("#addModal").modal("show");
+        /*发送ajax请求 获取一级分类全部数据，通过模板渲染*/
+        $.ajax({
+            type:"get",
+            url:"/category/queryTopCategoryPaging",
+            data:{
+                page:1,
+                pageSize:100
+            },
+            dataType:"json",
+            success:function (info) {
+                console.log(info);
+                var htnlStr = template( "dropdownTpl",info );
+                $(".dropdown-menu").html(htnlStr);
+            }
+        })
+    });
+
+
+    /*3通过事件委托，给dropdown-menu 下所有的a 绑定点击事件*/
+    $(".dropdown-menu").on("click","a",function () {
+        var txt = $(this).text();
+
+        $("#dropdownText").text(txt);
+        /*获取选中的id*/
+        var id =$(this).data("id");
+        //id设置给input
+        $('[name="categoryId"]').val(id);
+    });
+
+    /*4利用文件上传插件，进行文件上传初始化*/
+    $("#fileupload").fileupload({
+        //返回数据格式
+        dataType:"json",
+        //图片上传后，回调函数，
+        done:function (e, data) {
+            console.log(data.result.picAddr);
+
+            var imgUrl=data.result.picAddr;
+            $('#imgBox img').attr("src",imgUrl);
+            /*将图片地址设置给隐藏的input*/
+            $('[name="brandLogo"]').val(imgUrl);
+        }
+    })
+
+
 });
