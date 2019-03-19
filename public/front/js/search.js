56fdd8b507f2a35617d67a703c77d78c12f6392f
790573490 <790573490@qq.com>
2019-03-19 18:07:06 +0800

    搜索中心-删除功能，添加功能完成

--- public/front/js/search.js
+++ public/front/js/search.js
@@ -4,7 +4,7 @@ $(function () {
     //通过 search_list 进行读取或者设置属性
 
 
-    // var arr=['耐克','阿迪王','耐克王','新百伦'];
+    // var arr=['耐克','阿迪王','耐克王','新百伦','奔驰','宝马','奥迪'];
     // var json = JSON.stringify(arr);
     // localStorage.setItem('search_list',json);
 
@@ -41,11 +41,96 @@ $(function () {
     *       清空历史记录：removeItem()
     * */
     $(".lt_history").on("click",".btn_empty",function () {
-        //清空记录
-        localStorage.removeItem("search_list");
-        //渲染界面
-        render();
+        //添加mui确认框
+        mui.confirm("你确认要清空历史记录吗？","温馨提示",["取消","确认"],function (e) {
+            // e.index 可以获取点击的按钮的索引
+            console.log(e);
+            if(e.index ===1){
+                //清空记录
+                localStorage.removeItem("search_list");
+                //渲染界面
+                render();
+            }
+        })
+
+    });
+
+
+
+    /*
+    * 功能3：删除单条历史记录
+    * 1.注册委托点击事件，
+    * 2.将下角标存在删除按钮上，获取下角标
+    * 3.从本地存储中读取数组
+    * 4.通过下标，从数组中，将对应项删除 splice
+    * 5.将修改后的数组。装成jsonstr，存储到本地中
+    * 6页面渲染
+    * */
+    $(".lt_history").on("click",".btn_del",function () {
+
+        var that = this;
+        //添加删除确认框
+        mui.confirm("您确认删除该条记录吗？","温馨提示",["取消","确认"],function (e) {
+            if(e.index===1){
+                //获取下角标
+                var index = $(that).data("index");
+                //获取数组
+                var arr = getHistory();
+                //根据下角标。删除对应项
+                // arr.splice(从哪儿开始，删除几项，添加的项1，添加的项2)
+                arr.splice(index, 1);
+                //转成jsonstr，存储到本地去
+                localStorage.setItem("search_list",JSON.stringify( arr ));
+                //页面重新渲染
+                render();
+            }
+        })
+
     })
+
+
+
+    /*
+    * 功能4：添加历史记录功能
+    * 1.给搜索按钮，添加点击事件，
+    * 2.获取输入框的值
+    * 3.获取本地历史中存的数组
+    * 3.往数组的最前面追加
+    * 5.转成jsonstr。将修改后的存储到本地中
+    * 6.页面重新渲染
+    * */
+    $(".search_btn").click(function () {
+        var key = $(".search_input").val().trim();
+        if( key === ""){
+            alert("请输入搜索内容");
+            return;
+        }
+        //获取数组
+        var arr = getHistory();
+        /*
+        * 需求1：如果有重复的，现将重复的删除，将这项添加到最前面
+        *       2:超度不能超过十个
+        * */
+        var index = arr.indexOf(key);
+        // console.log(index);
+        if( index != -1){
+            arr.splice(index,1);
+        }
+        if(arr.length >=10){
+            //删除最后一项
+            arr.pop();
+        }
+
+
+
+        //    往数组最前面添加
+        arr.unshift(key);
+    //    转成json。存到本地
+        localStorage.setItem("search_list",JSON.stringify(arr));
+        render();
+        $(".search_input").val('');
+    })
+
 
 
 
